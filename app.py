d9347539257c50f8d5aa9b1e76370687305d7f98
liptika <liptikadhal@gmail.com>
2022-11-03 00:45:32 +0530

    app.py added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return 'Starting ML Project'
+    return 'Starting the ML Project'
 
 
 if __name__ == "__main__":
