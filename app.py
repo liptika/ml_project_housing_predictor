0f109b0c69dafbb3fd645085371ad9a103e6ac67
liptika <liptikadhal@gmail.com>
2022-11-04 20:05:25 +0530

    exception added

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import sys
 app = Flask(__name__)
 from housing.logger import logging
+from housing.exception import HousingException
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    logging.info("We are testing logging")
+    try:
+        raise("we are rasing custom exception")
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging")
     return 'looger done'
 
 
